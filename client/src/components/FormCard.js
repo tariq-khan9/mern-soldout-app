6138f5d38775d7fea2165d67fe11671a1f915b9d
tariq-khan9 <tariqkhan.cs9@gmail.com>
2023-04-11 20:08:37 +0500

    just random

--- client/src/components/FormCard.js
+++ client/src/components/FormCard.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import {Typography, TextField} from '@mui/material';
-
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 const bull = (
   <Box
     component="span"
@@ -17,27 +20,62 @@ const bull = (
 
 export default function BasicCard() {
 
-    function handleInput(){
+  const formDataInitial = {
+    product: '',
+    price: 0,
+    quantity: 0,
+    date: '',
 
+  }
+
+  const [formData , setFormData] = useState(formDataInitial)
+
+  function handleInput(e){
+    setFormData({...formData, [e.target.name]: e.target.value});
+  
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault();
+    const res = await fetch("http://localhost:5000/sold", {
+      method: 'post',
+      body: JSON.stringify(formData),
+      headers: {
+        'content-type': 'application/json',
+      }
+      
+    })
+    if(res.ok){
+      setFormData(formDataInitial);
+      
+      
     }
+    
+    
+  }
 
   return (
     <Card sx={{ minWidth: 275 }}>
       <CardContent>
-      <form >
+      <form onSubmit={handleSubmit} >
            
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" type='text' name='product' onChange={handleInput} placeholder='Enter Product' />
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" type="number" name='price' onChange={handleInput} placeholder='Enter Price' />
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" type='number' name='quantity' onChange={handleInput} placeholder='Quantity' />
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" placeholder='Enter Product' />
+            <TextField id="outlined-basic" style={{margin: '20px'}} size='small' label="Product" variant="outlined" type='text' name='product' onChange={handleInput} placeholder='Enter Product' />
+            <TextField id="outlined-basic" style={{margin: '20px'}} size='small' label="Price" variant="outlined" type="number" name='price' onChange={handleInput} placeholder='Enter Price' />
+            <TextField id="outlined-basic" style={{margin: '20px'}} size='small' label="Quantity" variant="outlined" type='number' name='quantity' onChange={handleInput} placeholder='Quantity' />
+            
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Controlled picker"
+                  
+                  onChange={(newValue) => setValue(newValue)}
+                />
+            </LocalizationProvider>
+
             <input  type='date' name='date' onChange={handleInput} />
             <button type='submit'>Submit</button>
 
         </form>
       </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,34 +1,15 @@
 
 import React from 'react';
+import FormCard from './FormCard.js'
 import { useState, useEffect} from 'react';
-import FormCard from './FormCard.js';
 
 const Home = () => {
-    const today = new Date();
-    const day = today.getDate();
-    const month = today.getMonth();
-    const year = today.getFullYear();
-
-    const formDataInitial = {
-      product: '',
-      price: 0,
-      quantity: 0,
-      date: `${year}-${month+1}-${day}`
-
-    }
-
-    const [formData , setFormData] = useState(formDataInitial)
-
+  
     const [soldData, setSoldData] = useState([]);
 
     useEffect(()=> {
       fetchSold()
     }, []);
-
-    function handleInput(e){
-      setFormData({...formData, [e.target.name]: e.target.value});
-    
-    }
 
     async function fetchSold(){
       const res = await fetch("http://localhost:5000/sold");
@@ -36,37 +17,10 @@ const Home = () => {
       setSoldData(data);
     }
 
-    async function handleSubmit(e){
-        e.preventDefault();
-        const res = await fetch("http://localhost:5000/sold", {
-          method: 'post',
-          body: JSON.stringify(formData),
-          headers: {
-            'content-type': 'application/json',
-          }
-          
-        })
-        if(res.ok){
-          setFormData(formDataInitial);
-          fetchSold();
-          
-        }
-        
-        
-    }
-    console.log(soldData);
-  return (   
+   return (   
     <div>
       <FormCard/>
-        <form onSubmit={handleSubmit}>
-            <input  type='text' name='product' onChange={handleInput} placeholder='Enter Product' />
-            <input type="number" name='price' onChange={handleInput} placeholder='Enter Price'  />
-            <input type='number' name='quantity' onChange={handleInput} placeholder='Quantity' />
-            <input  type='date' name='date' onChange={handleInput} />
-            <button type='submit'>Submit</button>
-        </form>
-        <hr/>
-        <section>
+        <section style={{marginTop:'50px'}}>
           <table>
             <thead>
               <td>Product</td>
@@ -89,8 +43,6 @@ const Home = () => {
             </tbody>
           </table>
         </section>
-
-
     </div>
   )
 }
